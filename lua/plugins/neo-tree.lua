d3f18223448db6596433284daf9c5c238968fcf7
ianm1837 <ian@iwm.dev>
2024-02-21 00:01:30 +0000

    auto close neo-tree; set hotkey to toggle

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -33,7 +33,7 @@ return {
         {text = "󰌵", texthl = "DiagnosticSignHint"})
 
       require("neo-tree").setup({
-        close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
         popup_border_style = "rounded",
         enable_git_status = true,
         enable_diagnostics = true,
@@ -178,7 +178,7 @@ return {
         filesystem = {
           filtered_items = {
             visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = true,
+            hide_dotfiles = false,
             hide_gitignored = true,
             hide_hidden = true, -- only works on Windows for hidden files/directories
             hide_by_name = {
@@ -300,6 +300,6 @@ return {
         }
       })
 
-      vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+      vim.cmd([[nnoremap \ :Neotree toggle<cr>]])
     end
 }
